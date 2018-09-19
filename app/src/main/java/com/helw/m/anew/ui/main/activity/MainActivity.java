1a7dfc4432cb2a0415beeab370eea836afc3f181
wangjun <2209119121@qq.com>
2018-09-19 10:57:37 +0800

    提交GlideManger

--- app/src/main/java/com/helw/m/anew/ui/main/activity/MainActivity.java
+++ app/src/main/java/com/helw/m/anew/ui/main/activity/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends BaseActivity{
     }
 
     public void initView() {
-        // TODO: 2018/9/18 提交测试22
 //        getPresenter().getData();
 //        getPresenter().getData2();
 //        getPresenter().setDatas(getPresenter().getDataSingle());
